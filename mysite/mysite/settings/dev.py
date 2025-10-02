6dd18f7b5c1d059a876f0dfb5f522e6258aeb6d0
NellitG <kamaunelson157@gmail.com>
2025-10-02 11:37:10 +0300

    Initial setup and database migration and blog registration

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ochcm2o*_^wj8%mavda2n6)n07tlsv^&=*eu1#fqgtn(_rg7*x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
