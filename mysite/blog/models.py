643ab4d6cb319f941beb44aade0e48dbfea4e3a4
NellitG <kamaunelson157@gmail.com>
2025-10-02 13:57:59 +0300

    Create model and made initial configuration

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.search import index
+# from wagtail.images.edit_handlers import FieldPanel
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+
 
 # Create your models here.
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+    ]
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        FieldPanel('image'),
+        FieldPanel('tags'),
+    ]
